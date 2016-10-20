da1ecb9a56b7aeb74544368bb8d5c371d9e0e6bb
ZiqueYuutaka <izferren@gmail.com>
2016-10-19 22:51:30 -0400

    fixed forms

--- BigBucksBank/WithdrawalForm.Designer.cs
+++ BigBucksBank/WithdrawalForm.Designer.cs
@@ -31,6 +31,14 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.btnWithdrawal = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbSavings = new System.Windows.Forms.RadioButton();
+            this.rbChecking = new System.Windows.Forms.RadioButton();
+            this.rtbMessage = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbAmount = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label2
@@ -55,12 +63,78 @@
             // 
             // btnWithdrawal
             // 
-            this.btnWithdrawal.Location = new System.Drawing.Point(218, 404);
+            this.btnWithdrawal.Location = new System.Drawing.Point(99, 419);
             this.btnWithdrawal.Name = "btnWithdrawal";
             this.btnWithdrawal.Size = new System.Drawing.Size(171, 44);
             this.btnWithdrawal.TabIndex = 7;
             this.btnWithdrawal.Text = "WITHDRAWAL";
             this.btnWithdrawal.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(340, 419);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(171, 44);
+            this.btnExit.TabIndex = 13;
+            this.btnExit.Text = "EXIT";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbSavings);
+            this.groupBox1.Controls.Add(this.rbChecking);
+            this.groupBox1.Location = new System.Drawing.Point(193, 138);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(257, 47);
+            this.groupBox1.TabIndex = 20;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Accounts";
+            // 
+            // rbSavings
+            // 
+            this.rbSavings.AutoSize = true;
+            this.rbSavings.Location = new System.Drawing.Point(170, 15);
+            this.rbSavings.Name = "rbSavings";
+            this.rbSavings.Size = new System.Drawing.Size(63, 17);
+            this.rbSavings.TabIndex = 14;
+            this.rbSavings.TabStop = true;
+            this.rbSavings.Text = "Savings";
+            this.rbSavings.UseVisualStyleBackColor = true;
+            // 
+            // rbChecking
+            // 
+            this.rbChecking.AutoSize = true;
+            this.rbChecking.Location = new System.Drawing.Point(52, 15);
+            this.rbChecking.Name = "rbChecking";
+            this.rbChecking.Size = new System.Drawing.Size(70, 17);
+            this.rbChecking.TabIndex = 13;
+            this.rbChecking.TabStop = true;
+            this.rbChecking.Text = "Checking";
+            this.rbChecking.UseVisualStyleBackColor = true;
+            // 
+            // rtbMessage
+            // 
+            this.rtbMessage.Location = new System.Drawing.Point(193, 280);
+            this.rtbMessage.Name = "rtbMessage";
+            this.rtbMessage.Size = new System.Drawing.Size(258, 115);
+            this.rtbMessage.TabIndex = 19;
+            this.rtbMessage.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(190, 232);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Amount:";
+            // 
+            // tbAmount
+            // 
+            this.tbAmount.Location = new System.Drawing.Point(242, 225);
+            this.tbAmount.Name = "tbAmount";
+            this.tbAmount.Size = new System.Drawing.Size(209, 20);
+            this.tbAmount.TabIndex = 17;
             // 
             // WithdrawalForm
             // 
@@ -68,11 +142,18 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(640, 532);
             this.ControlBox = false;
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.rtbMessage);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbAmount);
+            this.Controls.Add(this.btnExit);
             this.Controls.Add(this.btnWithdrawal);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "WithdrawalForm";
             this.Text = "WithdrawalForm";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -83,5 +164,12 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnWithdrawal;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbSavings;
+        private System.Windows.Forms.RadioButton rbChecking;
+        private System.Windows.Forms.RichTextBox rtbMessage;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbAmount;
     }
 }
